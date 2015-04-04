f8e9db5d5085fa90b6400739fde4360787d9b78f
shakilaust <shakil.austcse27@gmail.com>
2015-04-04 18:59:47 +0600

    Create 191.cpp

---
+++ 191.cpp
@@ -0,0 +1,175 @@
+
+//BISMILLAHIRRAHMANIRRAHIM
+/*
+ manus tar shopner soman boro
+ Author :: Shakil Ahmed
+.............AUST_CSE27.........
+ prob   ::
+ Type   ::
+ verdict::
+ */
+
+#include <bits/stdc++.h>
+#include <tr1/unordered_map>
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define ff first
+#define ss second
+#define re return
+#define QI queue<int>
+#define SI stack<int>
+#define SZ(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define sqr(x) ((x) * (x))
+#define ms(a,b) memset((a),(b),sizeof(a))
+#define G() getchar()
+#define MAX3(a,b,c) max(a,max(b,c))
+#define II ( { int a ; read(a) ; a; } )
+#define LL ( { Long a ; read(a) ; a; } )
+#define DD ({double a; scanf("%lf", &a); a;})
+
+double const EPS=3e-8;
+using namespace std;
+
+#define FI freopen ("input.txt", "r", stdin)
+#define FO freopen ("output.txt", "w", stdout)
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
+
+// For loop
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+template< class T > T gcd(T a, T b)
+{
+    return (b != 0 ? gcd<T>(b, a%b) : a);
+}
+template< class T > T lcm(T a, T b)
+{
+    return (a / gcd<T>(a, b) * b);
+}
+
+//Fast Reader
+template<class T>inline bool read(T &x)
+{
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9')
+    {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+struct Point
+{
+    int x;
+    int y;
+};
+
+// Given three colinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+bool onSegment(Point p, Point q, Point r)
+{
+    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+        q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
+       return true;
+
+    return false;
+}
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int rotation(Point p, Point q, Point r)
+{
+    // See 10th slides from following link for derivation of the formula
+    // http://www.dcs.gla.ac.uk/~pat/52233/slides/Geometry1x1.pdf
+    int val = (q.y - p.y) * (r.x - q.x) -
+              (q.x - p.x) * (r.y - q.y);
+
+    if (val == 0) return 0;  // colinear
+
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+bool intersect(Point p1, Point q1, Point p2, Point q2)
+{
+    // Find the four orientations needed for general and
+    // special cases
+    int o1 = rotation(p1, q1, p2);
+    int o2 = rotation(p1, q1, q2);
+    int o3 = rotation(p2, q2, p1);
+    int o4 = rotation(p2, q2, q1);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+    // p1, q1 and p2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+     // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+    return false; // Doesn't fall in any of the above cases
+}
+bool needswap(Point A , Point B)
+{
+    if( A.x > B.x ) return 1 ;
+    return 0 ;
+}
+int main()
+{
+    // I will always use scanf and printf
+    // May be i won't be a good programmer but i will be a good human being
+    Point l , r , A , B , C , D;
+    int cs , t = II ;
+    for ( cs = 1 ; cs <= t ; cs++ )
+    {
+        cin >> l.x >> l.y >> r.x >> r.y >> A.x >> A.y >> C.x >> C.y ;
+        if(needswap(l,r)) swap(l,r);
+        if(needswap(A,C)) swap(A,C);
+        B.x = C.x ;
+        B.y = A.y ;
+        D.x = A.x ;
+        D.y = C.y ;
+        if( intersect(l,r,A,B) || intersect(l,r,B,C) || intersect(l,r,C,D) || intersect(l,r,D,A) || ( rotation(A,B,l) == rotation(B,C,l) && rotation(C,D,l) == rotation(B,C,l) && rotation(D,A,l) == rotation(A,B,l)))
+            puts("T");
+        else puts("F");
+    }
+
+
+    return 0;
+}
+
