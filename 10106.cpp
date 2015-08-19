24f76dfd55bb781fffe3132389835a88c7b90b4b
shakil ahmed <shakil.austcse27@gmail.com>
2015-08-19 17:58:49 +0600

    BigInteger

---
+++ 10106.cpp
@@ -0,0 +1,213 @@
+//BISMILLAHIRRAHMANIRRAHIM
+/*
+ manus tar shopner soman boro
+ all my suceesses are dedicated to my parents
+ Author :: Shakil Ahmed
+.............AUST_CSE27.........
+ prob   ::
+ Type   ::
+ verdict::
+ */
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define ff first
+#define ss second
+#define re return
+#define QI queue<int>
+#define SI stack<int>
+#define SZ(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define sqr(x) ((x) * (x))
+#define ms(a,b) memset((a),(b),sizeof(a))
+#define G() getchar()
+#define MAX3(a,b,c) max(a,max(b,c))
+#define II ( { int a ; read(a) ; a; } )
+#define LL ( { Long a ; read(a) ; a; } )
+#define DD ({double a; scanf("%lf", &a); a;})
+
+double const EPS=3e-8;
+using namespace std;
+
+#define FI freopen ("input.txt", "r", stdin)
+#define FO freopen ("output.txt", "w", stdout)
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
+
+// For loop
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+template< class T > T gcd(T a, T b) { return (b != 0 ? gcd<T>(b, a%b) : a); }
+template< class T > T lcm(T a, T b) { return (a / gcd<T>(a, b) * b); }
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=getchar();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=getchar();}for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0'; x*=sgn; return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+/* **************************************  My code start here ****************************************** */
+struct Bigint {
+    // representations and structures
+    string a; // to store the digits
+    int sign; // sign = -1 for negative numbers, sign = 1 otherwise
+
+    // constructors
+    Bigint() {} // default constructor
+    Bigint( string b ) { (*this) = b; } // constructor for string
+
+    // some helpful methods
+    int size() { // returns number of digits
+        return a.size();
+    }
+    Bigint inverseSign() { // changes the sign
+        sign *= -1;
+        return (*this);
+    }
+    Bigint normalize( int newSign ) { // removes leading 0, fixes sign
+        for( int i = a.size() - 1; i > 0 && a[i] == '0'; i-- )
+            a.erase(a.begin() + i);
+        sign = ( a.size() == 1 && a[0] == '0' ) ? 1 : newSign;
+        return (*this);
+    }
+
+    // assignment operator
+    void operator = ( string b ) { // assigns a string to Bigint
+        a = b[0] == '-' ? b.substr(1) : b;
+        reverse( a.begin(), a.end() );
+        this->normalize( b[0] == '-' ? -1 : 1 );
+    }
+
+    // conditional operators
+    bool operator < ( const Bigint &b ) const { // less than operator
+        if( sign != b.sign ) return sign < b.sign;
+        if( a.size() != b.a.size() )
+            return sign == 1 ? a.size() < b.a.size() : a.size() > b.a.size();
+        for( int i = a.size() - 1; i >= 0; i-- ) if( a[i] != b.a[i] )
+            return sign == 1 ? a[i] < b.a[i] : a[i] > b.a[i];
+        return false;
+    }
+    bool operator == ( const Bigint &b ) const { // operator for equality
+        return a == b.a && sign == b.sign;
+    }
+
+
+
+    // mathematical operators
+    Bigint operator + ( Bigint b ) { // addition operator overloading
+        if( sign != b.sign ) return (*this) - b.inverseSign();
+        Bigint c;
+        for(int i = 0, carry = 0; i<a.size() || i<b.size() || carry; i++ ) {
+            carry+=(i<a.size() ? a[i]-48 : 0)+(i<b.a.size() ? b.a[i]-48 : 0);
+            c.a += (carry % 10 + 48);
+            carry /= 10;
+        }
+        return c.normalize(sign);
+    }
+    Bigint operator - ( Bigint b ) { // subtraction operator overloading
+        if( sign != b.sign ) return (*this) + b.inverseSign();
+        int s = sign; sign = b.sign = 1;
+        if( (*this) < b ) return ((b - (*this)).inverseSign()).normalize(-s);
+        Bigint c;
+        for( int i = 0, borrow = 0; i < a.size(); i++ ) {
+            borrow = a[i] - borrow - (i < b.size() ? b.a[i] : 48);
+            c.a += borrow >= 0 ? borrow + 48 : borrow + 58;
+            borrow = borrow >= 0 ? 0 : 1;
+        }
+        return c.normalize(s);
+    }
+    Bigint operator * ( Bigint b ) { // multiplication operator overloading
+        Bigint c("0");
+        for( int i = 0, k = a[i] - 48; i < a.size(); i++, k = a[i] - 48 ) {
+            while(k--) c = c + b; // ith digit is k, so, we add k times
+            b.a.insert(b.a.begin(), '0'); // multiplied by 10
+        }
+        return c.normalize(sign * b.sign);
+    }
+    Bigint operator / ( Bigint b ) { // division operator overloading
+        if( b.size() == 1 && b.a[0] == '0' ) b.a[0] /= ( b.a[0] - 48 );
+        Bigint c("0"), d;
+        for( int j = 0; j < a.size(); j++ ) d.a += "0";
+        int dSign = sign * b.sign; b.sign = 1;
+        for( int i = a.size() - 1; i >= 0; i-- ) {
+            c.a.insert( c.a.begin(), '0');
+            c = c + a.substr( i, 1 );
+            while( !( c < b ) ) c = c - b, d.a[i]++;
+        }
+        return d.normalize(dSign);
+    }
+    Bigint operator % ( Bigint b ) { // modulo operator overloading
+        if( b.size() == 1 && b.a[0] == '0' ) b.a[0] /= ( b.a[0] - 48 );
+        Bigint c("0");
+        b.sign = 1;
+        for( int i = a.size() - 1; i >= 0; i-- ) {
+            c.a.insert( c.a.begin(), '0');
+            c = c + a.substr( i, 1 );
+            while( !( c < b ) ) c = c - b;
+        }
+        return c.normalize(sign);
+    }
+
+
+
+    // output method
+    void print() {
+        if( sign == -1 ) putchar('-');
+        for( int i = a.size() - 1; i >= 0; i-- ) putchar(a[i]);
+    }
+};
+
+
+int main()
+{
+
+
+     Bigint a , b , c ;
+     string s1 ,s2 ;
+     while( cin >> s1 >> s2 )
+     {
+         a = s1 ;
+         b = s2 ;
+         c = a * b ;
+         c.print();
+         puts("");
+     }
+    return 0 ;
+}
+/*
+5
+0 0
+2 0
+2 2
+1 1
+0 2
+5
+0 0
+0 2
+1 3
+2 2
+2 0
+0
+
+*/
