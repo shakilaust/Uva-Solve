d2d6fa69b474addad7ad36e5600296d4d2e6871c
shakil ahmed <shakil.austcse27@gmail.com>
2015-08-29 10:29:02 +0600

    probability

---
+++ 11181.cpp
@@ -0,0 +1,132 @@
+//BISMILLAHIRRAHMANIRRAHIM
+/*
+ manus tar shopner soman boro
+ all my suceesses are dedicated to my parents
+ Author :: Shakil Ahmed
+.............AUST_CSE27.........
+ prob   ::
+ Type   ::
+ verdict::
+ */
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+
+// Macro
+#define pi acos(-1.0)
+#define ff first
+#define ss second
+#define re return
+#define QI queue<int>
+#define SI stack<int>
+#define SZ(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define sq(a) ((a)*(a))
+#define distance(a,b) (sq(a.x-b.x) + sq(a.y-b.y))
+#define eps (1e-9)
+#define iseq(a,b) (fabs(a-b)<eps)
+#define eq(a,b) iseq(a,b)
+#define ms(a,b) memset((a),(b),sizeof(a))
+#define G() getchar()
+#define MAX3(a,b,c) max(a,max(b,c))
+#define II ( { int a ; read(a) ; a; } )
+#define LL ( { Long a ; read(a) ; a; } )
+#define DD ({double a; scanf("%lf", &a); a;})
+
+
+using namespace std;
+
+#define FI freopen ("input.txt", "r", stdin)
+#define FO freopen ("output.txt", "w", stdout)
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
+
+// For loop
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+template< class T > T gcd(T a, T b) { return (b != 0 ? gcd<T>(b, a%b) : a); }
+template< class T > T lcm(T a, T b) { return (a / gcd<T>(a, b) * b); }
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=getchar();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=getchar();}for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0'; x*=sgn; return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+/* **************************************  My code start here ****************************************** */
+const int NX = 25 ;
+
+double tot[ NX ] , p [ NX ] ;
+
+int n , r ;
+
+bool input()
+{
+    n = II , r = II ;
+    if( n == 0 && r == 0 ) return 0 ;
+    rep ( i , n )
+    {
+        tot[i] = 0 ;
+        scanf("%lf",&p[i]);
+    }
+    return 1 ;
+}
+
+double DP( int pos , double P , int taken )
+{
+    if( pos == n )
+    {
+        if( taken == r ) return P ;
+        else return 0.0 ;
+    }
+    double ans1 = 0 , ans2 = 0 ;
+    if( taken < r ) // its possible to buy
+    {
+       ans1 = DP( pos + 1 , P * p[pos] , taken + 1 );
+       tot[ pos ] += ans1 ;
+    }
+    ans2 = DP( pos + 1 , P * ( 1 - p[pos] ) , taken );
+    return ans1 + ans2 ;
+
+}
+int main()
+{
+   // I will always use scanf and printf
+       int cs = 1 ;
+
+       while(input()) {
+       double ans = DP( 0 , 1 ,  0 );
+       printf("Case %d:\n",cs++);
+       rep( i , n ) printf("%lf\n",tot[i]/ans);
+   }
+    return 0;
+}
+/*
+2 2 2
+10000 1
+20000 2
+30000 1 2
+40000 1 2
+0 0 0
+
+*/
